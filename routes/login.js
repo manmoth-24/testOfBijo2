937c7e9465939fc15fc49a18eba13e4648ee212f
Manmoth <129261117+manmoth-24@users.noreply.github.com>
2024-07-17 16:22:04 +0900

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require("sqlite3")
+
+var db = new sqlite3.Database("mydb.db")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', {
+  res.render('login/index', {
     messageForLogin: ''
   });
 });
+
+router.get('/makeAccount', (req, res, next)=>{
+  res.render('login/makeAccount', {
+
+  })
+})
+
+router.post('/makeAccount', (req, res, next) =>{
+  var accountName = req.body["name"]
+  var accountNewPass = req.body["newPass"]
+
+  var q = 'select * from accountsData where account="' + accountName + '"'
+  var q2 = 'insert into accountsData (account, password) values (?, ?)'
+  
+  db.serialize(()=>{
+    db.get(q, [], (err, rows) => {
+      console.log(rows)
+      if (!err){
+        if (rows == undefined || rows == []){
+          db.run(q2, accountName, accountNewPass)
+          req.session.accountID = accountName
+          res.render("login/loginResult", {
+            title: 'アカウントの作成が完了しました。',
+            message: 'ID:' + accountName + '<br>でログインしています。↑',
+            linksA: '<a href="/">ホームに戻る</a>'
+          })
+        }else{
+          res.render("login/loginResult", {
+            title: 'そのアカウントは既に存在します。',
+            message: 'アカウント名を変えてください。',
+            linksA: '<a href="./makeAccount">もう一度</a>'
+          })
+        }
+      }else{
+        console.error(err.message)
+      }
+        
+    })
+  })
+})
 
 module.exports = router;
--- routes/loginResult.js
+++ routes/loginResult.js
@@ -4,6 +4,10 @@ var router = express.Router();
 var id = 'MA2024';
 var idPass = '20240706'
 
+const sqlite3 = require("sqlite3")
+
+var db = new sqlite3.Database("mydb.db")
+
 router.get('/', (req, res, next) =>{
     res.send('ログインしてください。')
 })
@@ -12,21 +16,35 @@ router.post('/', (req, res, next) =>{
   var account = req.body['accountID']
   var pass = req.body['accountPass']
   
-  if (account == id && pass == idPass){
-    req.session.accountID = account;
-    res.render('loginResult', {
-        title: 'ログインが完了しました。',
-        message: 'ID:' + account + '<br>でログインしています。↑',
-        linksA: '<a href="/">ホームに戻る</a>'
-    });
-    //セッションからアカウントIDに書き込む
-  }else{
-    res.render('loginResult', {
-        title: 'ログインエラー',
-        message: 'IDまたはパスワードが間違っています。<br>',
-        linksA : '<a href="/login">ログインページに戻る</a>'
+  var q = 'select * from accountsData where account="' + account + '"'
+  db.serialize(() =>{
+    db.get(q, [], (err, rows) =>{
+      if (!err){
+        if (rows == undefined){
+          res.render('login/loginResult', {
+            title: 'ログインエラー',
+            message: 'IDまたはパスワードが間違っています。<br>',
+            linksA : '<a href="./login">ログインページに戻る</a>'
+          })
+        }else if(rows.password == pass){
+          req.session.accountID = account;
+          res.render('login/loginResult', {
+              title: 'ログインが完了しました。',
+              message: 'ID:' + account + '<br>でログインしています。↑',
+              linksA: '<a href="/">ホームに戻る</a>'
+          });
+        }else{
+          res.render('login/loginResult', {
+            title: 'ログインエラー',
+            message: 'IDまたはパスワードが間違っています。<br>',
+            linksA : '<a href="./login">ログインページに戻る</a>'
+          })
+        }
+      }else{
+        console.error(err.message)
+      }
     })
-  }
+  })
 })
 
 module.exports = router;
